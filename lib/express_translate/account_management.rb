3b5c8937f4a6cfac1f168aa06234567283ae5ad8
Karl <karl@rubify.com>
2014-06-30 10:48:15 +0700

    Comment code (Documents)

--- lib/express_translate/account_management.rb
+++ lib/express_translate/account_management.rb
@@ -9,16 +9,19 @@ module ExpressTranslate
     @primary = "username"
     @attr = "password", "token"
     
+    # Modify account when add (Encoding password)
     def self.add(params)
       params[:password] = self.encoding(params[:password])
       super(params)
     end
     
+    # Find account with token, check status login bt cookie
     def self.find_by_token(token)
       _find = self.all.select{|s| (s["token"] == token)}
       return _find.count > 0 ? _find[0] : nil
     end
     
+    # Ecoding password
     def self.encoding(string)
       encode = Base64.encode64(string)
       encode = encode.split("=").join()
